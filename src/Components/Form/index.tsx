a58a4b9aca5a63489beaf53b8bd12a1bd27e2a78
Daniel Ribeiro <academicodan@gmail.com>
2021-06-08 09:59:57 -0300

    update front styles

--- src/Components/Form/index.tsx
+++ src/Components/Form/index.tsx
@@ -4,10 +4,13 @@ import { useState, useEffect } from 'react'
 import FormControl from '@material-ui/core/FormControl'
 import FormLabel from '@material-ui/core/FormLabel'
 import RadioGroup from '@material-ui/core/RadioGroup'
-import { FormControlLabel, InputLabel, Radio, Select, MenuItem, Button } from '@material-ui/core'
-
-
-export function Form() {
+import { FormControlLabel, InputLabel, Radio, Select, MenuItem, Button, IconButton } from '@material-ui/core'
+import CloseRoundedIcon from '@material-ui/icons/CloseRounded'
+import Upload from '../Upload/index'
+import FileList from "../FileList/index"
+import { uniqueId } from 'lodash'
+import filesize from 'filesize'
+export function Form({ isMenuVisible, setIsMenuVisible }) {
 
   const [tema, setTema] = useState<string | unknown>("");
   const [nomeEquipe, setNomeEquipe] = useState<string>("");
@@ -16,16 +19,35 @@ export function Form() {
   const [integrantes, setIntegrantes] = useState<string>("");
   const [quesito, setQuesito] = useState<string>("");
   const [ideia, setIdeia] = useState<string>("");
-  const [dadosColetados, setDados] = useState([])
+  const [dadosColetados, setDados] = useState([]);
+
+  const [stateFile, setStateFile] = useState({
+    uploadedFiles: []
+  })
+
+  
+  const handleUpload = (files) => {
+    const uploadedFiles = files.map(file => ({
+      file, //info para enviar viar api
+      id: uniqueId(),
+      name: file.name,
+      readableSize: filesize(file.size),
+    }))
+    // não sobrepõe o state e adiciona outros a lista
+    setStateFile({
+      uploadedFiles: stateFile.uploadedFiles.concat(uploadedFiles)
+    });
+  }
+
 
   function ColetarDados(dados) {
     const newDados = [...dadosColetados, dados]
     setDados(newDados);
-    console.log({dadosColetados})
+    console.log({ dadosColetados })
     limparCampos()
   }
 
-  function limparCampos(){
+  function limparCampos() {
     setTema("");
     setEmail("");
     setNomeEquipe("");
@@ -35,120 +57,138 @@ export function Form() {
     setIdeia("");
   }
 
+  const handleDelete = async (id) => {
+   setStateFile({
+     uploadedFiles: stateFile.uploadedFiles.filter(file => file.id !== id),
+   })
+  }
+
+
   return (
-    <div className={style.formContainer}>
-      <h1 id="title" className={style.title}>Cadastro de ideias</h1>
-      <p id="caption" className={style.caption}>Preencha as informações</p>
-      <form action="" 
-        onSubmit={(event) => {
-          event.preventDefault();
-          ColetarDados({tema, nomeEquipe, responsavel, email, integrantes, quesito, ideia})
-        }}  
-      >
+    <>
+      <div className={isMenuVisible ? style.formContainerOpen : style.formContainerClose}>
+        <IconButton onClick={() => setIsMenuVisible(!isMenuVisible)}>
+          <CloseRoundedIcon />
+        </IconButton>
+        <h1 id="title" className={style.title}>Cadastro de ideias</h1>
+        <p id="caption" className={style.caption}>Preencha as informações</p>
+        <form action=""
+          onSubmit={(event) => {
+            event.preventDefault();
+            ColetarDados({ tema, nomeEquipe, responsavel, email, integrantes, quesito, ideia })
+          }}
+        >
 
-        <FormControl  required fullWidth margin="normal">
-          <InputLabel >Selecione o tema</InputLabel>
-          <Select
-            value={tema}
-            name="tema"
-            onChange={(event) => {
-              setTema(event.target.value)
-            }}
-          >
-            <MenuItem value="Tema 1">Theme name</MenuItem>
-            <MenuItem value="Tema 2">Theme name</MenuItem>
-            <MenuItem value="Tema 3">Theme name</MenuItem>
-            <MenuItem value="Tema 4">Theme name</MenuItem>
-            <MenuItem value="Tema 5">Theme name</MenuItem>
+          <FormControl required fullWidth margin="normal">
+            <InputLabel >Selecione o tema</InputLabel>
+            <Select
+              value={tema}
+              name="tema"
+              onChange={(event) => {
+                setTema(event.target.value)
+              }}
+            >
+              <MenuItem value="Tema 1">Theme name</MenuItem>
+              <MenuItem value="Tema 2">Theme name</MenuItem>
+              <MenuItem value="Tema 3">Theme name</MenuItem>
+              <MenuItem value="Tema 4">Theme name</MenuItem>
+              <MenuItem value="Tema 5">Theme name</MenuItem>
             </Select>
-        </FormControl >
+          </FormControl >
 
-        <TextField
-          value={nomeEquipe}
-          id="nomeEquipe"
-          label="Nome da equipe"
-          name="nomeEquipe"
-          fullWidth
-          margin="normal"
-          onChange={(event) => {
-            setNomeEquipe(event.target.value)
-          }}
-        />
+          <TextField
+            value={nomeEquipe}
+            id="nomeEquipe"
+            label="Nome da equipe"
+            name="nomeEquipe"
+            fullWidth
+            margin="normal"
+            onChange={(event) => {
+              setNomeEquipe(event.target.value)
+            }}
+          />
 
-        <TextField
-          value={responsavel}
-          id="responsavel"
-          label="Responsavel"
-          name="responsavel"
-          fullWidth
-          required
-          margin="normal"
-          onChange={(event) => {
-            setResponsavel(event.target.value)
-          }}
-        />
+          <TextField
+            value={responsavel}
+            id="responsavel"
+            label="Responsavel"
+            name="responsavel"
+            fullWidth
+            required
+            margin="normal"
+            onChange={(event) => {
+              setResponsavel(event.target.value)
+            }}
+          />
 
-        <TextField
-          value={email}
-          onChange={(event) => {
-            setEmail(event.target.value)
-          }}
-          id="email"
-          label="Email"
-          name="email"
-          fullWidth
-          type="email"
-          required
-          margin="normal"
-        />
+          <TextField
+            value={email}
+            onChange={(event) => {
+              setEmail(event.target.value)
+            }}
+            id="email"
+            label="Email"
+            name="email"
+            fullWidth
+            type="email"
+            required
+            margin="normal"
+          />
 
-        <FormControl required  margin="normal">
-          <FormLabel >Numero de integrantes incluindo você</FormLabel>
-          <RadioGroup aria-label="" name="" value={integrantes} onChange={(event) => {
+          <FormControl required margin="normal">
+            <FormLabel >Numero de integrantes incluindo você</FormLabel>
+            <RadioGroup aria-label="" name="" value={integrantes} onChange={(event) => {
               setIntegrantes(event.target.value)
             }}>
-            <FormControlLabel value="unico" control={<Radio color="primary"/>} label="Só você" />
-            <FormControlLabel value="1-3" control={<Radio color="primary"/>} label="1 - 3" />
-            <FormControlLabel value="3-5" control={<Radio color="primary"/>} label="3 - 5" />
-            <FormControlLabel value="5-10" control={<Radio color="primary"/>} label="5 - 10" />
-            <FormControlLabel value="mais" control={<Radio color="primary"/>} label="mais" />
-          </RadioGroup>
-        </FormControl>
+              <FormControlLabel value="unico" control={<Radio color="primary" />} label="Só você" />
+              <FormControlLabel value="1-3" control={<Radio color="primary" />} label="1 - 3" />
+              <FormControlLabel value="3-5" control={<Radio color="primary" />} label="3 - 5" />
+              <FormControlLabel value="5-10" control={<Radio color="primary" />} label="5 - 10" />
+              <FormControlLabel value="mais" control={<Radio color="primary" />} label="mais" />
+            </RadioGroup>
+          </FormControl>
 
-        <FormControl component="fieldset" fullWidth required margin="normal">
-          <FormLabel component="legend">Em qual quesito sua ideia melhor se encaixa</FormLabel>
-          <RadioGroup aria-label="Quesitos" name="quesito" value={quesito}             onChange={(event) => {
+          <FormControl component="fieldset" fullWidth required margin="normal">
+            <FormLabel component="legend">Em qual quesito sua ideia melhor se encaixa</FormLabel>
+            <RadioGroup aria-label="Quesitos" name="quesito" value={quesito} onChange={(event) => {
               setQuesito(event.target.value)
             }}>
-            <FormControlLabel value="Quesito 1" control={<Radio color="primary"/>} label="Tenho um problema e preciso de uma solução" />
-            <FormControlLabel value="Quesito 2" control={<Radio color="primary"/>} label="Tenho uma solução para um problema" />
-            <FormControlLabel value="Quesito 3" control={<Radio color="primary"/>} label="Quero desenvolver um produto" />
-            <FormControlLabel value="Quesito 4" control={<Radio color="primary"/>} label="Outros" />
-          </RadioGroup>
-        </FormControl>
+              <FormControlLabel value="Quesito 1" control={<Radio color="primary" />} label="Tenho um problema e preciso de uma solução" />
+              <FormControlLabel value="Quesito 2" control={<Radio color="primary" />} label="Tenho uma solução para um problema" />
+              <FormControlLabel value="Quesito 3" control={<Radio color="primary" />} label="Quero desenvolver um produto" />
+              <FormControlLabel value="Quesito 4" control={<Radio color="primary" />} label="Outros" />
+            </RadioGroup>
+          </FormControl>
 
-        <TextField
-          value={ideia}
-          onChange={(event) => {
-            setIdeia(event.target.value)
-          }}
-          name="ideia"
-          id="ideia"
-          fullWidth
-          margin="normal"
-          label="Descrição da ideia"
-          placeholder="Detalhe aqui sua ideia..."
-          multiline
-          variant="outlined"
-          required
-        />
-
-        <div className={style.buttonSubmite}>
-        <Button variant="contained" color="primary" fullWidth type="submit">
-          CADASTRAR
-        </Button>
-        </div>
-      </form>
-    </div>
+          <TextField
+            value={ideia}
+            onChange={(event) => {
+              setIdeia(event.target.value)
+            }}
+            name="ideia"
+            id="ideia"
+            fullWidth
+            margin="normal"
+            label="Descrição da ideia"
+            placeholder="Detalhe aqui sua ideia..."
+            multiline
+            variant="outlined"
+            required
+          />
+          <div className={style.upload}>
+            <Upload onUpload={handleUpload} />
+          </div>
+          {!!stateFile.uploadedFiles.length && (
+            <div className={style.upload}>
+              <FileList files={stateFile.uploadedFiles} onDelete={handleDelete}/>
+            </div>
+          )
+          }
+          <Button variant="contained" color="primary" fullWidth type="submit">
+            CADASTRAR
+          </Button>
+        </form>
+      </div>
+    </>
   )
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CardTheme } from '../Components/CardTheme'
 import { Form } from '../Components/Form'
 import { Header } from '../Components/Header'
 import style from './styles.module.scss'
+
+
 export default function UserPage() {
+   const [isMenuVisible, setIsMenuVisible] = useState(false)
+
   return (
     <>
-      <div className={style.wrapper}>
-        <main>
-          <Header />
-          <div className={style.home}>
-            <CardTheme />
-          </div>
-        </main>
-        <Form />
-      </div>
+        <div className={style.wrapper}>
+          <main>
+            <Header isMenuVisible={isMenuVisible} setIsMenuVisible={setIsMenuVisible} />
+            <div className={style.home}>
+              <CardTheme />
+            </div>
+          </main>
+          <Form isMenuVisible={isMenuVisible} setIsMenuVisible={setIsMenuVisible} />
+        </div>
     </>
   )
+}
+
+
+function setMenu({state}) {
+  const[isMenu, setIsMenu] = useState<boolean>()
+  setIsMenu(state)
 }
